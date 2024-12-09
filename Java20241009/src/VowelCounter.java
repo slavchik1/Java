12a564491677db296fb9519ea3ccbe4c2553960b
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2024-12-09 14:02:19 +0200

    More " "s `+ some changes

--- Java20241009/src/VowelCounter.java
+++ Java20241009/src/VowelCounter.java
@@ -2,7 +2,7 @@ public class VowelCounter {
     public static int countVowels(String input) {
         char[] vowels = {'a', 'u', 'i', 'e', 'o', 'A', 'U', 'I', 'E', 'O'};
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == ) {
+            if (input.charAt(i) == 'a') {
 
             }
         }
